8808335f8df071c2589dca47c0c60f2075c9fec4
LogicInGeneral2 <ajmaj578@gmail.com>
2025-05-26 10:17:06 +0800

    [1.1.2]: - Updated settings.py for production

--- backend/backend/settings/prod.py
+++ backend/backend/settings/prod.py
@@ -15,8 +15,10 @@ DATABASES = {
     )
 }
 
-# Static files for production
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # WhiteNoise middleware for serving static files
